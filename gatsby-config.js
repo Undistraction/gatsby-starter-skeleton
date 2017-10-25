649c10b6b92358165c45ea9a3c1d78b4fcfa6309
Pedr Browne <pedr.browne@gmail.com>
2017-10-25 09:52:11 +0100

    fix: Add back responsive iframe support

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,7 @@ module.exports = {
       resolve: 'gatsby-transformer-remark',
       options: {
         plugins: [
+          'gatsby-remark-responsive-iframe',
           {
             resolve: 'gatsby-remark-responsive-image',
             options: {
