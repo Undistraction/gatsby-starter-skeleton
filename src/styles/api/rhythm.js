4b53e61e69a15bfa5b3ff7f723e29a80aa232d18
Pedr Browne <pedr.browne@gmail.com>
2018-04-23 13:16:21 +0100

    feat: Add demo generation

--- src/styles/api/rhythm.js
+++ src/styles/api/rhythm.js
@@ -3,11 +3,6 @@ import { multiply, compose } from 'ramda';
 import { validatePxValues, validateNumber } from '../validations';
 import { toUnitlessValue } from '../utils/units';
 
-// const DEFAULT_CONFIG = {
-//   rhythmUnit: 20.8, // 16px * 1.3
-//   opticalAdjustment: 0.05,
-// };
-
 export default (rhythmUnit, output) => {
   validatePxValues([rhythmUnit]);
   const rhythmUnitInUnitlessPx = toUnitlessValue(rhythmUnit);
