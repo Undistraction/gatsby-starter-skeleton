b1d1d083f542ff83bcc370beea8a587a3eb6744c
Pedr Browne <pedr.browne@gmail.com>
2018-05-04 13:10:48 +0100

    refactor: remove cssapi debugging

--- scripts/demo-create.sh
+++ scripts/demo-create.sh
@@ -1,13 +1,15 @@
-echo 'Building Demo'
+echo '💀 Building Demo'
 rm -rf ./demo/project
 mkdir ./demo/project
 # Create a new build of the demo project and deploy to github pages
 cd ./demo
 # Create a new project using gatsby-skeleton as a starter
 gatsby new project https://github.com/undistraction/gatsby-skeleton-starter
+echo '💀 Created New Project Based on Starter'
 # Copy demo content into the generated project
 rm -rf ./project/src/content
 cp -r ./content ./project/src/content
+echo '💀 Replaced Default Content'
 # Insert path prefix option into Gatsby config file
 sed -i '' "s#// pathPrefix: '/example'#pathPrefix: '/gatsby-skeleton-starter'#g" ./project/gatsby-config.js
-echo 'Deloyed Demo to Github Pages'
+echo '💀 Set path prefixes in gatsby-config'
