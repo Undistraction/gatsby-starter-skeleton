fddf8e2dc1d2e6cd1371e76cf1fb0eaea4512acf
Pedr Browne <pedr.browne@gmail.com>
2018-05-23 17:08:41 +0100

    fix(images): remove redundant multiplication

--- src/config/plugins/remark/images.js
+++ src/config/plugins/remark/images.js
@@ -2,7 +2,7 @@ module.exports = images => ({
   resolve: `gatsby-remark-images`,
   options: {
     // The maximum width of images on the site.
-    maxWidth: images.maxWidth * 2,
+    maxWidth: images.maxWidth,
     // Set the quality of the processed images
     quality: images.quality,
     linkImagesToOriginal: false,
