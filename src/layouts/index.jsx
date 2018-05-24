e05be62d8b160c57112f5ff9e787e66fa4ccde19
Pedr Browne <pedr.browne@gmail.com>
2018-05-24 23:14:40 +0100

    fix: Stray bracket in theme provider

--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -1,11 +1,11 @@
-import graphql from 'graphql';
-import PropTypes from 'prop-types';
-import React from 'react';
-import { ThemeProvider } from 'styled-components';
-import Site from '../components/Site';
-import theApi from '../components/styles/api';
-import '../components/styles/global.css';
-import '../components/styles/prism-greyscale-theme.css';
+import graphql from 'graphql'
+import PropTypes from 'prop-types'
+import React from 'react'
+import { ThemeProvider } from 'styled-components'
+import Site from '../components/Site'
+import theApi from '../components/styles/api'
+import '../components/styles/global.css'
+import '../components/styles/prism-greyscale-theme.css'
 
 // Note: VSCode currently doesn't respect object prop one-liner and removes
 // import, so we need to rename the prop. This is already fixed in the repo and
@@ -15,10 +15,10 @@ const theme = {
 }
 
 const IndexLayout = ({ children, data }) => (
-    <ThemeProvider theme={theme}>
-      <Site data={data}>{children}</Site>
-    </ThemeProvider>
-}
+  <ThemeProvider theme={theme}>
+    <Site data={data}>{children}</Site>
+  </ThemeProvider>
+)
 
 IndexLayout.propTypes = {
   children: PropTypes.func.isRequired,
