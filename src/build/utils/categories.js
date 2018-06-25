a4d682aeada69cb44d9ca08fa8a681460f951552
Pedr Browne <pedr.browne@gmail.com>
2018-06-25 10:56:05 +0100

    refactor: Remove forced uncategorised category

--- src/build/utils/categories.js
+++ src/build/utils/categories.js
@@ -1,14 +1,12 @@
 const { CATEGORIES_PATH_ELEMENT } = require(`../const/urls`)
-const { UNCATEGORISED_CATEGORY } = require(`../const/strings`)
 const { compact, appendFlipped } = require(`ramda-adjunct`)
-const { apply, append, pipe, path, map, uniq } = require(`ramda`)
+const { apply, pipe, path, map, uniq } = require(`ramda`)
 const { toSlug } = require(`./url`)
 const urlJoin = require(`url-join`)
 
 const collectUniqueCategories = pipe(
   map(path([`node`, `fields`, `category`])),
   compact,
-  append(UNCATEGORISED_CATEGORY),
   uniq
 )
 
