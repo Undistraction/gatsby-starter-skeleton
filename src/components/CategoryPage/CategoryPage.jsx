a66bb0f490ee6f40e69640bad5af20b4c73d5be2
Pedr Browne <pedr.browne@gmail.com>
2018-06-25 10:47:50 +0100

    debug: Add logging for demo build error

--- src/components/CategoryPage/CategoryPage.jsx
+++ src/components/CategoryPage/CategoryPage.jsx
@@ -15,6 +15,11 @@ const CategoryPage = ({ data, pathContext }) => {
   const metadata = categoryPageMetadata(data)
   const { category } = pathContext
 
+  console.log(`---------------------------------------------------------------`)
+  console.log(`Data`, data)
+  console.log(`Path Context`, pathContext)
+  console.log(`---------------------------------------------------------------`)
+
   const articles = markdownItemNodes(data)
 
   return (
